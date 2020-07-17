3eb93fc5c59c6b0f9928c8bea1fd5162ccc83deb
Dane Harnett <dane_harnett@flightcentre.com>
2020-07-17 13:08:20 +1000

    Install Material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo();
-
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild?.(jssStyles);
+  }, []);
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>You Done Yet</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import dynamic from "next/dynamic";
+import Button from "@material-ui/core/Button";
 
-const IndexPage = () => <div>You Done Yet</div>;
+const IndexPage = () => (
+  <div>
+    You Done Yet
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+  </div>
+);
 
 export default dynamic(() => Promise.resolve(IndexPage), {
   ssr: false,
