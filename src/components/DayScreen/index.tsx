4c42aeb20b41e8bcce4236c1dfd3b5a3f0f502b7
Dane Harnett <dane_harnett@flightcentre.com>
2020-08-07 18:45:15 +1000

    Fix build by moving non 'page' files out of pages folder

---
+++ src/components/DayScreen/index.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { add, sub } from "date-fns";
+import AppHeader from "../AppHeader";
+import NavigationTabs from "../NavigationTabs";
+import SelectedDate from "../SelectedDate";
+import HabitList from "../HabitList";
+
+export const DayScreen = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  return (
+    <div data-testid="day-screen">
+      <AppHeader />
+      <NavigationTabs />
+      <SelectedDate
+        selectedDate={selectedDate}
+        onPrevious={() => setSelectedDate(sub(selectedDate, { days: 1 }))}
+        onNext={() => setSelectedDate(add(selectedDate, { days: 1 }))}
+      />
+      <HabitList selectedDate={selectedDate} />
+    </div>
+  );
+};
+
+export default DayScreen;
